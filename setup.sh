69526788649aae15da6b62b3c3de19a0531817eb
mitto nagisa <mitto@mittostar.info>
2012-09-30 19:36:16 +0900

    Update: setup.sh
    ・すでに存在していたファイルがシンボリックじゃない場合のみバックアップするようにした
    ・シンボリックリンクがすでにあるときは何もしないようにした

--- setup.sh
+++ setup.sh
@@ -4,9 +4,14 @@ dotfiles=( .zshrc .tmux.conf .screenrc .vimrc .hgrc .hgignore_global )
 
 for file in ${dotfiles[@]}
 do
-  if [ -e $HOME/$file ]; then
+  if [ -e $HOME/$file -a ! -L $HOME/$file ]; then
+    echo "backup file : ${file}"
     mv $HOME/$file $HOME/$file.bak
   fi
 
-  ln -s $HOME/dotfiles/$file $HOME/$file
+  if [ ! -L $HOME/$file ]; then
+    echo "new symbolic link : ${file}"
+    ln -s $HOME/dotfiles/$file $HOME/$file
+  fi
+
 done
