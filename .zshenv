f18bcfeafde0737954e5794277cf4491c321f20e
mitto nagisa <mitto@mittostar.info>
2012-11-10 17:16:54 +0900

    add rsense setup and configures

--- .zshenv
+++ .zshenv
@@ -3,3 +3,5 @@ if [ -e $HOME/.pythonz ]; then
   [[ -s $HOME/.pythonz/etc/bashrc ]] && source $HOME/.pythonz/etc/bashrc
   export PATH=$HOME/.pythonz/pythons/CPython-2.7.3/bin:$PATH
 fi
+
+export RSENSE_HOME=$HOME/opt/rsense-0.3
--- setup.sh
+++ setup.sh
@@ -18,21 +18,37 @@ done
 
 
 if [ ! -e $HOME/.vim/.backup ]; then
-  echo created vim backup folder : $HOME/.vim/.backup
+  echo create vim backup folder : $HOME/.vim/.backup
   mkdir $HOME/.vim/.backup
 fi
 
 if [ ! -e $HOME/.vim/.swap ]; then
-  echo created vim swap folder : $HOME/.vim/.swap
+  echo create vim swap folder : $HOME/.vim/.swap
   mkdir $HOME/.vim/.swap
 fi
 
 if [ ! -e $HOME/bin ]; then
-  echo created bin folder : $HOME/bin
+  echo create bin folder : $HOME/bin
   mkdir $HOME/bin
 fi
 
 if [ ! -e $HOME/opt ]; then
-  echo created opt folder : $HOME/opt
+  echo create opt folder : $HOME/opt
   mkdir $HOME/opt
+fi
+
+if [ ! -e $HOME/opt/rsense-0.3 ]; then
+  echo Download rsense : http://cx4a.org/pub/rsense/rsense-0.3.zip
+  curl http://cx4a.org/pub/rsense/rsense-0.3.zip > $HOME/opt/rsense-0.3.zip
+  cd $HOME/opt
+  echo Unzip rsense-0.3.zip file
+  unzip rsense-0.3.zip
+  echo Delete rsense-0.3.zip file
+  rm rsense-0.3.zip
+  cd $HOME
+fi
+
+if [ ! -e $HOME/.rsense ]; then
+  echo Configure rsense
+  ruby $HOME/opt/rsense-0.3/etc/config.rb > $HOME/.rsense
 fi
