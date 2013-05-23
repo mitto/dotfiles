f793b3082470a85212ca0d816396aa4ebfe5c6cc
mitto nagisa <mitto@mittostar.info>
2013-05-23 15:33:21 +0900

    powerline 移行に伴い.tmux.d周りのファイルを削除

--- setup.sh
+++ setup.sh
@@ -36,30 +36,3 @@ if [ ! -e $HOME/opt ]; then
   echo create opt folder : $HOME/opt
   mkdir $HOME/opt
 fi
-
-for file in `ls $HOME/dotfiles/.tmux.d`
-do
-  if [ ! -L $HOME/bin/$file ]; then
-    echo "new symbolic link: $file"
-    ln -s $HOME/dotfiles/.tmux.d/$file $HOME/bin/$file
-  fi
-done
-
-if [ ! -e $HOME/opt/rubyref ]; then
-  echo "start rubyref install"
-  cd $HOME/opt
-  wget "http://doc.ruby-lang.org/archives/201208/ruby-refm-1.9.3-dynamic-20120829.tar.gz" -O $HOME/opt/rubyref.tar.gz
-  tar zxvf rubyref.tar.gz
-  rm $HOME/opt/rubyref.tar.gz
-  mv $HOME/opt/ruby-refm-1.9.3-dynamic-20120829 $HOME/opt/rubyref
-fi
-
-if [ ! -e $HOME/bin/refe ]; then
-  echo "create refe commands"
-  cat <<EOL > $HOME/bin/refe
-#!/bin/sh
-exec ruby -Ke -I ~/opt/rubyref/bitclust/lib ~/opt/rubyref/bitclust/bin/refe -d ~/opt/rubyref/db-1_9_3 "\$@"
-EOL
-  chmod 775 $HOME/bin/refe
-  cd $HOME
-fi
