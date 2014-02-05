3daf0325d1ef3781e87c57412c1a3c991f0f2bcc
mitto nagisa <mitto@mittostar.info>
2014-02-05 11:23:41 +0900

    add git config files

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dotfiles=( .zprofile .zshrc .zshenv .zsh.d .tmux.conf .screenrc .vimrc .vim .hgrc .mercurial .gemrc )
+dotfiles=( .zprofile .zshrc .zshenv .zsh.d .tmux.conf .screenrc .vimrc .vim .hgrc .mercurial .gemrc .gitconfig .gitignore_global )
 
 for file in ${dotfiles[@]}
 do
