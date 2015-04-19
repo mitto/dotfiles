88f2c9072747fb964447bbc6d01c83177be11d60
mitto nagisa <mitto@mittostar.info>
2015-04-20 00:36:29 +0900

    /usr/local change order path

--- shell.d/initializer.sh
+++ shell.d/initializer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PATH=$HOME/.rbenv/bin:$HOME/bin:$PATH:/usr/local/bin:/usr/local/sbin
+PATH=$HOME/.rbenv/bin:$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH
 
 if [ -e $HOME/.rbenv ]; then
   eval "$(rbenv init -)"
