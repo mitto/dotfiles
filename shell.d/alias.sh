3459ee126fc2f4540697fd6da77ea2c6c7a59ec2
mitto nagisa <mitto@mittostar.info>
2015-04-20 00:37:10 +0900

    move grep options

--- shell.d/alias.sh
+++ shell.d/alias.sh
@@ -17,3 +17,35 @@ alias nvim="vim -u NONE -N"
 
 alias l='ls -laF'
 alias ll='l'
+
+
+# grep
+if type ggrep > /dev/null 2>&1; then
+  alias grep=ggrep
+fi
+
+grep_version="$(grep --version | head -n 1 | sed -e 's/^[^0-9.]*\([0-9.]*\)[^0-9.]*$/\1/')"
+export GREP_OPTIONS
+GREP_OPTIONS="--binary-files=without-match"
+case "$grep_version" in
+    1.*|2.[0-4].*|2.5.[0-3])
+  ;;
+    *)
+  ### grep 2.5.4以降のみの設定
+  ### grep対象としてディレクトリを指定したらディレクトリ内を再帰的にgrepする。
+  GREP_OPTIONS="--directories=recurse $GREP_OPTIONS"
+  ;;
+esac
+
+GREP_OPTIONS="--exclude=\*.tmp $GREP_OPTIONS"
+
+if grep --help 2>&1 | grep -q -- --exclude-dir; then
+  GREP_OPTIONS="--exclude-dir=.svn $GREP_OPTIONS"
+  GREP_OPTIONS="--exclude-dir=.git $GREP_OPTIONS"
+  GREP_OPTIONS="--exclude-dir=.deps $GREP_OPTIONS"
+  GREP_OPTIONS="--exclude-dir=.libs $GREP_OPTIONS"
+fi
+
+if grep --help 2>&1 | grep -q -- --color; then
+  GREP_OPTIONS="--color=auto $GREP_OPTIONS"
+fi
