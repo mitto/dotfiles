df732f1774e50dea73c81e700de91c658dfae4d1
mitto nagisa <mitto@mittostar.info>
2015-04-26 23:12:01 +0900

    fix awscli completion enable only bash

--- shell.d/complete.sh
+++ shell.d/complete.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-if [ -f /usr/bin/aws ]; then
-  complete -C aws_completer aws
+# for awscli
+AWS_COMMAND_PATH=$(which aws)
+if [ $? -eq 0 ]; then
+  [ $(basename $SHELL) = 'bash' ] && complete -C aws_completer aws
 fi
