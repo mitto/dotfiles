58560660ca415a33bd615e8c8d8942fed3694d8f
mitto nagisa <mitto@mittostar.info>
2016-09-18 07:21:10 +0900

    setup: refactor and add .brewfile

--- setup.sh
+++ setup.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 
+function create-folder () {
+  local folder_path=$1
+  if [ ! -e $folder_path ]; then
+    echo create bin folder : $folder_path
+    mkdir $folder_path
+  fi
+}
+
+function create-symlink () {
+  local src_path=$HOME/dotfiles/$1
+  local dst_path=$HOME/$1
+  if [ -e $dst_path -a ! -L $dst_path ]; then
+    echo "backup file : $dst_path"
+    mv $dst_path $dst_path.bak
+  fi
+
+  if [ ! -L $dst_path ]; then
+    echo "new symbolic link : $dst_path"
+    ln -s $src_path $dst_path
+  fi
+}
+
 dotfiles=( .zshrc .zshenv .tmux.conf .screenrc .vimrc .gemrc .gitconfig .gitignore_global .hgrc .hgignore_global )
 
 for file in ${dotfiles[@]}
 do
-  if [ -e $HOME/$file -a ! -L $HOME/$file ]; then
-    echo "backup file : ${file}"
-    mv $HOME/$file $HOME/$file.bak
-  fi
-
-  if [ ! -L $HOME/$file ]; then
-    echo "new symbolic link : ${file}"
-    ln -s $HOME/dotfiles/$file $HOME/$file
-  fi
+  create-symlink $file
 done
 
-if [ ! -e $HOME/bin ]; then
-  echo create bin folder : $HOME/bin
-  mkdir $HOME/bin
-fi
+create-folder $HOME/bin
+
+case ${OSTYPE} in
+  darwin*)
+    create-symlink .brewfile
+    ;;
+esac
